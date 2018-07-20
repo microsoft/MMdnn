85d373b4553b2bc12addd6e7e2d955b66a032f2e
kitstar <kitseraph@outlook.com>
2018-07-20 07:00:10 +0000

    Implement tf LRN layer in alexnet.

--- mmdnn/conversion/tensorflow/tensorflow_emitter.py
+++ mmdnn/conversion/tensorflow/tensorflow_emitter.py
@@ -498,12 +498,13 @@ def KitModel(weight_file = None):
 
 
     def emit_LRN(self, IR_node):
-        self.add_body(1, "{:<15} = tf.nn.lrn({}, {}, alpha = {}, beta = {}, name = '{}')".format(
+        self.add_body(1, "{:<15} = tf.nn.lrn({}, {}, alpha={}, beta={}, bias={}, name='{}')".format(
             IR_node.variable_name,
             self.parent_variable_name(IR_node),
             IR_node.get_attr('size') - 1,
-            IR_node.layer.attr['alpha'].f / (IR_node.layer.attr['size'].i * 2 - 1),
+            IR_node.get_attr('alpha') / (IR_node.get_attr('size') * 2 - 1),
             IR_node.get_attr('beta'),
+            IR_node.get_attr('bias', 0),
             IR_node.name))
 
 
