f8aeb010430f757ac15876fcdd4774f05b70c4c7
kitstar <kitseraph@outlook.com>
2018-08-30 04:41:38 +0000

    fix tensorflow/keras/mxnet traivs error.

--- tests/test_keras.py
+++ tests/test_keras.py
@@ -1,9 +1,26 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-
+import os
+import six
 from test_conversion_imagenet import TestModels
 
+def get_test_table():
+    if six.PY2:
+        return {
+            'keras' : {
+                'vgg19'        : [TestModels.CaffeEmit, TestModels.CntkEmit, TestModels.CoreMLEmit, TestModels.KerasEmit, TestModels.MXNetEmit, TestModels.PytorchEmit, TestModels.TensorflowEmit],
+                'inception_v3' : [TestModels.CaffeEmit, TestModels.CntkEmit, TestModels.CoreMLEmit, TestModels.KerasEmit, TestModels.MXNetEmit, TestModels.PytorchEmit, TestModels.TensorflowEmit],
+                'resnet50'     : [TestModels.CaffeEmit, TestModels.CntkEmit, TestModels.CoreMLEmit, TestModels.KerasEmit, TestModels.MXNetEmit, TestModels.PytorchEmit, TestModels.TensorflowEmit],
+                'densenet'     : [TestModels.CaffeEmit, TestModels.CntkEmit, TestModels.CoreMLEmit, TestModels.KerasEmit, TestModels.MXNetEmit, TestModels.PytorchEmit, TestModels.TensorflowEmit],
+                'xception'     : [TestModels.TensorflowEmit, TestModels.KerasEmit, TestModels.CoreMLEmit],
+                'mobilenet'    : [TestModels.CoreMLEmit, TestModels.KerasEmit, TestModels.TensorflowEmit], # TODO: MXNetEmit
+                # 'nasnet'       : [TensorflowEmit, KerasEmit, CoreMLEmit],
+            }},
+    else:
+        return None
+
+
 def test_keras():
-    tester = TestModels()
+    tester = TestModels(get_test_table())
     tester._test_function('keras', tester.KerasParse)
--- tests/test_mxnet.py
+++ tests/test_mxnet.py
@@ -1,9 +1,30 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-
+import os
+import six
 from test_conversion_imagenet import TestModels
 
+def get_test_table():
+    TRAVIS_CI = os.environ.get('TRAVIS')
+    if not TRAVIS_CI or TRAVIS_CI.lower() != 'true':
+        return None
+
+    ONNX = os.environ.get('TEST_ONNX')
+    if ONNX and ONNX.lower() == 'true': return None
+
+    if six.PY2: return None
+
+    return { 'mxnet' : {
+        'imagenet1k-inception-bn'      : [CaffeEmit, CntkEmit, CoreMLEmit, KerasEmit, MXNetEmit, PytorchEmit, TensorflowEmit],
+        'imagenet1k-resnet-18'         : [CaffeEmit, CntkEmit, CoreMLEmit, KerasEmit, MXNetEmit, PytorchEmit, TensorflowEmit],
+        'imagenet1k-resnet-152'        : [CaffeEmit, CntkEmit, CoreMLEmit, KerasEmit, MXNetEmit, PytorchEmit, TensorflowEmit],
+        'squeezenet_v1.1'              : [CaffeEmit, CntkEmit, CoreMLEmit, KerasEmit, MXNetEmit, PytorchEmit, TensorflowEmit],
+        'imagenet1k-resnext-101-64x4d' : [CaffeEmit, CntkEmit, CoreMLEmit, MXNetEmit, PytorchEmit, TensorflowEmit], # Keras is ok but too slow
+        'imagenet1k-resnext-50'        : [CaffeEmit, CntkEmit, CoreMLEmit, KerasEmit, MXNetEmit, PytorchEmit, TensorflowEmit],
+    }}
+
+
 def test_mxnet():
-    tester = TestModels()
+    tester = TestModels(get_test_table())
     tester._test_function('mxnet', tester.MXNetParse)
