825db569ccf5e9c243b07c266aea3b8ec85acc3b
HX Lin <>
2019-01-08 15:31:02 +0800

    Revert test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
 # Abort on Error
 set -e
-python -m pytest -s -q tests/test_$1.py
+
+export PING_SLEEP=60s
+export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export BUILD_OUTPUT=$WORKDIR/build.out
+
+touch $BUILD_OUTPUT
+
+dump_output() {
+   echo Tailing the last 100 lines of output:
+   tail -100 $BUILD_OUTPUT
+}
+error_handler() {
+  echo ERROR: An error was encountered with the build.
+  dump_output
+  exit 1
+}
+
+# If an error occurs, run our error handler to output a tail of the build
+trap 'error_handler' ERR
+
+# Set up a repeating loop to send some output to Travis.
+
+bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
+PING_LOOP_PID=$!
+
+# My build is using maven, but you could build anything with this, E.g.
+python -m pytest -s -q tests/test_$1.py >> $BUILD_OUTPUT 2>&1
+
+# The build finished without returning an error so dump a tail of the output
+dump_output
+
+# nicely terminate the ping output loop
+kill $PING_LOOP_PID
