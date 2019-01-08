c62bc991d65dd343350f064b98234bd1edf5199a
HX Lin <>
2019-01-08 12:22:09 +0800

    Fix test.sh for wrong path reference.

--- test.sh
+++ test.sh
@@ -29,13 +29,13 @@ PING_LOOP_PID=$!
 # My build is using maven, but you could build anything with this, E.g.
 python tests/gen_test.py -o tests/
 if [ $# -eq 3 ]; then
-    test_file=mytests/test_$1_$2_$3.py
+    test_file=tests/test_$1_$2_$3.py
     if [ ! -f $test_file ]; then
         echo "File $test_file does not exist."
         exit 1
     fi
 elif [ $# -eq 2 ]; then
-    test_file=mytests/test_$1_*_$2.py
+    test_file=tests/test_$1_*_$2.py
 else
 	echo "Wrong test arguments."
 	exit 1
