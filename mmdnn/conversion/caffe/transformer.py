2af4c146846ce990e160ae17066372eb30b364fd
Claudio Baecchi <claudio.baecchi@gmail.com>
2019-01-21 11:22:48 +0100

    preserve output order when converting batchnorm+scale layers

--- mmdnn/conversion/caffe/transformer.py
+++ mmdnn/conversion/caffe/transformer.py
@@ -176,8 +176,9 @@ class SubNodeFuser(object):
                 continue
             # Rewrite the fused node's children to its parent.
             for child in node.children:
-                child.parents = [(input, idx) for input, idx in child.parents if input != node]
-                child.add_parent(parent, from_output)
+                index = [n for n, (input, idx) in enumerate(child.parents) if input == node][0]
+                child.parents.pop(index)
+                child.add_parent(parent, from_output, index)
             # Disconnect the fused node from the graph.
             parent.children.remove(node)
             fused_nodes.append(node)
