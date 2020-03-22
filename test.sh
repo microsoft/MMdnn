237e1638017fa5c4bd0d2e49fb264c39047fc22e
HX Lin <>
2020-03-22 08:05:03 +0800

    Update.

--- test.sh
+++ test.sh
@@ -9,8 +9,8 @@ export BUILD_OUTPUT=$WORKDIR/build.out
 touch $BUILD_OUTPUT
 
 dump_output() {
-   echo Tailing the last 100 lines of output:
-   tail -100 $BUILD_OUTPUT
+   echo Tailing the last 500 lines of output:
+   tail -500 $BUILD_OUTPUT
 }
 error_handler() {
   echo ERROR: An error was encountered with the build.
@@ -27,8 +27,7 @@ bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
 PING_LOOP_PID=$!
 
 # My build is using maven, but you could build anything with this, E.g.
-#python -m pytest -s -q tests/test_$1.py >> $BUILD_OUTPUT 2>&1
-python -m pytest -s -q tests/test_$1.py 1>/dev/null
+python -m pytest -s -q tests/test_$1.py >> $BUILD_OUTPUT 2>&1
 
 # The build finished without returning an error so dump a tail of the output
 dump_output
